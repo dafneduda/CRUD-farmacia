21e17963c9dfb546def9934bbe24f0dd9877f753
Dafne <dafne.leslie@gmail.com>
2024-03-26 12:33:15 -0300

    CRUD do Produto

--- src/main/java/com/performancegoal/farmacia/model/Categoria.java
+++ src/main/java/com/performancegoal/farmacia/model/Categoria.java
@@ -1,11 +1,18 @@
 package com.performancegoal.farmacia.model;
 
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 @Entity
@@ -16,14 +23,18 @@ public class Categoria {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
-	@NotBlank(message = "O atributo Nome é Obrigatório")
+	@NotNull(message = "O atributo Nome é Obrigatório")
 	@Size(min = 3, max = 100, message = "O atributo Nome deve conter no mínimo 3 e no máximo 100 caracteres")
 	private String nome;
 
-	@NotBlank(message = "O atributo Descrição é Obrigatório")
+	@NotNull(message = "O atributo Descrição é Obrigatório")
 	@Size(min = 5, max = 255, message = "O atributo Descrição deve conter no mínimo 5 e no máximo 255 caracteres")
 	private String descricao;
-
+	
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "categoria", cascade = CascadeType.REMOVE)
+	@JsonIgnoreProperties("categoria")
+	private List<Produto>produto;
+	
 	public Long getId() {
 		return id;
 	}
@@ -46,6 +57,14 @@ public class Categoria {
 
 	public void setDescricao(String descricao) {
 		this.descricao = descricao;
+	}
+
+	public List<Produto> getProduto() {
+		return produto;
+	}
+
+	public void setProduto(List<Produto> produto) {
+		this.produto = produto;
 	}
 
 }
